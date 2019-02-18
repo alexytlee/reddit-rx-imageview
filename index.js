ddf430ae28af2ad9abf2440fbc963d89fb30bf6e
Alex Lee <alexytlee@gmail.com>
2019-02-18 11:05:13 -0800

    Simple Reddit imageviewer using Rx.js
    Signed-off-by: Alex Lee <alexytlee@gmail.com>

---
+++ index.js
@@ -0,0 +1,110 @@
+const nextButton = document.getElementById('next');
+const backButton = document.getElementById('back');
+const subSelect = document.getElementById('sub');
+const img = document.getElementById('img');
+const loading = document.getElementById('loading');
+
+const LOADING_ERROR_URL = './docs/error.png';
+const Observable = Rx.Observable;
+
+// function which returns an array of image URLs for a given reddit sub
+// getSubImages("pics") ->
+// [
+//   "https://upload.wikimedia.org/wikipedia/commons/3/36/Hopetoun_falls.jpg",
+//   "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg",
+//   ...
+// ]
+function getSubImages(sub) {
+	const cachedImages = localStorage.getItem(sub);
+	if (cachedImages) {
+		return Observable.of(JSON.parse(cachedImages));
+	} else {
+		const url = `https://www.reddit.com/r/${sub}/.json?limit=200&show=all`;
+
+		// defer ensure new Observable (and therefore) promise gets created
+		// for each subscription. This ensures functions like retry will
+		// issue additional requests.
+		return Observable.defer(() =>
+			Observable.fromPromise(
+				fetch(url)
+					.then(res => res.json())
+					.then(data => {
+						const images = data.data.children.map(
+							image => image.data.url
+						);
+						localStorage.setItem(sub, JSON.stringify(images));
+						return images;
+					})
+			)
+		);
+	}
+}
+
+const subs = Observable.concat(
+	Observable.of(subSelect.value),
+	Observable.fromEvent(subSelect, 'change').map(ev => ev.target.value)
+);
+
+const nexts = Observable.fromEvent(nextButton, 'click');
+
+const backs = Observable.fromEvent(backButton, 'click');
+
+const offsets = Observable.merge(nexts.map(() => 1), backs.map(() => -1));
+
+const indices = Observable.concat(
+	Observable.of(0),
+	offsets.scan((acc, curr) => acc + curr, 0)
+);
+
+const images = subs
+	.map(sub =>
+		getSubImages(sub)
+			.map(images =>
+				indices
+					.filter(index => index >= 0 && index < images.length)
+					.map(index => images[index])
+			)
+			.switch()
+			.map(preloadImage)
+			.switch()
+	)
+	.switch();
+
+const actions = Observable.merge(subs, nexts, backs);
+
+actions.subscribe(() => (loading.style.visibility = 'visible'));
+
+// Testing if the buttons work
+// subs.subscribe(data => alert(data));
+// images.subscribe(image => console.log(image));
+
+images.subscribe({
+	next(url) {
+		// hide the loading image
+		loading.style.visibility = 'hidden';
+
+		// set Image source to URL
+		img.src = url;
+	},
+	error(e) {
+		alert(
+			"I'm having trouble loading the images for that sub. Please wait a while, reload, and then try again later."
+		);
+	}
+});
+
+function preloadImage(src) {
+	return Observable.defer(() => {
+		const img = new Image();
+		const success = Observable.fromEvent(img, 'load').map(() => src);
+		const failure = Observable.fromEvent(img, 'error').map(
+			() => LOADING_ERROR_URL
+		);
+		img.src = src;
+
+		return Observable.merge(success, failure);
+	});
+}
+
+// observable that notfies whenever a user performs an action,
+// like changing the sub or navigating the images
